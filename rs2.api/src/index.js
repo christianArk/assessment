70c05afed6db9eb9e9fcf4c0df34661ce7e71417
Onyeneke Christian <onyenekechristian@gmail.com>
2022-04-15 07:06:08 +0100

    feat: allowed all origins for cors

--- rs2.api/src/index.js
+++ rs2.api/src/index.js
@@ -21,14 +21,18 @@ mongoose.connect(DB_URI, {
 seedUser()
 seedProducts()
 
-app.use(cors())
+app.use(cors(
+    {
+        origin: '*'
+    }
+))
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 app.use("/api", routes)
 
-app.get('/', (req, res) => { res.send("<ChetaJS/>, start building something we'd remember!")});
+app.get('/', (req, res) => { res.send("Start building something we'd remember!")});
 
 app.listen(PORT, () => { console.log(`Your app is running on port ${PORT}`)});
 
