2cdf647e2fd00975a7542b483a8fb0238a99dadf
Christian Rs2 <christianrs2@christians-MacBook-Pro.local>
2022-08-07 00:03:56 +0100

    configured jest

---
+++ rs2.client/tests/index.spec.js
@@ -0,0 +1,12 @@
+import { mount, shallowMount } from '@vue/test-utils'
+import Index from '../pages/index.vue'
+
+describe('Index.vue', () => {
+    let wrapper;
+
+    beforeEach(async () => {
+        wrapper = mount(Index);
+    });
+
+    it.todo('Should work');
+})
